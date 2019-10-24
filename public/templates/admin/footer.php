f9e139860142e9ee83e5c235cef1dbdc1f14769e
Le Hoang Long <long.bk.khmt@gmail.com>
2019-10-25 00:22:17 +0700

    save

--- public/templates/admin/footer.php
+++ public/templates/admin/footer.php
@@ -8,9 +8,7 @@
 Admin EShop
         </p>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="public/js/highcharts.src.js"></script>
-    <script src="//cdn.ckeditor.com/4.13.0/standard/ckeditor.js"></script>    
+    
     <?php
     if(($_GET['c']==''||$_GET['c']=='view')&&($_GET['a']==''||$_GET['a']=='index'))
     echo '<script src="public/js/filter-product-admin.js"></script>';
